21742a7fd7c1bc6f46aeea0b39f2800800c2eafd
Arosos <wierzba96@o2.pl>
2017-12-02 15:40:26 +0100

    Added rotation

--- Tankz/Assets/Scripts/PlayerController.cs
+++ Tankz/Assets/Scripts/PlayerController.cs
@@ -41,14 +41,27 @@ public class PlayerController : MonoBehaviour {
     void Move(Direction d)
     {
         Vector2Int move = Vector2Int.zero;
+        Quaternion rotate = new Quaternion();
         if (d == Direction.Down)
+        {
             move.y = -1;
+            rotate = Quaternion.AngleAxis(180, Vector3.forward);
+        }
         else if (d == Direction.Left)
+        {
             move.x = -1;
+            rotate = Quaternion.AngleAxis(90, Vector3.forward);
+        }
         else if (d == Direction.Up)
+        {
             move.y = 1;
+            rotate = Quaternion.AngleAxis(0, Vector3.forward);
+        }
         else if (d == Direction.Right)
+        {
             move.x = 1;
+            rotate = Quaternion.AngleAxis(-90, Vector3.forward);
+        }
         Vector3 t = node.transform.position;
         Debug.Log(((int)t.x / 32 + move.x) + "," + ((int)t.y / 32 + move.y));
         Node destinationNode = grid.nodes[(int)t.x  / 32 + move.x, (int)t.y / 32 + move.y];
@@ -58,6 +71,7 @@ public class PlayerController : MonoBehaviour {
             node = destinationNode;
             node.objectOnNode = gameObject;
             gameObject.transform.SetParent(node.transform);
+            transform.rotation = rotate;
             transform.localPosition = new Vector3();
         }
     }
