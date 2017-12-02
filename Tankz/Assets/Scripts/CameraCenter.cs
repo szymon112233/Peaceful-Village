dbe93b92fe9fec2b2d2667df0773e899d8ea32d2
Szymon Piórkowski <szymmon123@gmail.com>
2017-12-02 12:47:56 +0100

    Added basic grid Generation.

---
+++ Tankz/Assets/Scripts/CameraCenter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCenter : MonoBehaviour {
+
+    public float scrollSpeed = 10.0f;
+    public NodeGrid grid = null;
+
+    Camera mainCamera = null;
+    
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Start()
+    {
+        mainCamera.transform.position = new Vector3(grid.nodeSize * grid.gridSize.x /2, grid.nodeSize * grid.gridSize.y / 2, - 200);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 newVectror = mainCamera.transform.position;
+        newVectror.z += Input.mouseScrollDelta.y;
+        mainCamera.transform.position = newVectror;
+
+    }
+}
---
+++ Tankz/Assets/Scripts/NodeGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGrid : MonoBehaviour {
+
+    public Vector2Int gridSize = new Vector2Int();
+    public float nodeSize = 0.0f;
+    public GameObject nodePrefab = null;
+    public Node[,] nodes = null;
+
+	
+	void Start () {
+        GenerateGrid();
+	}
+	
+	void GenerateGrid()
+    {
+        nodes = new Node[gridSize.x, gridSize.y];
+        for (int i = (gridSize.y - 1) ; i>=0 ; i--)
+        {
+            for (int j = 0; j < gridSize.x; j++)
+            {
+                GameObject go = Instantiate(nodePrefab, new Vector3(j * nodeSize, i * nodeSize, 1.0f), new Quaternion(), gameObject.transform);
+                nodes[j, i] = go.GetComponent<Node>();
+            }
+        }
+        Debug.Log("Dupa");
+    }
+}
