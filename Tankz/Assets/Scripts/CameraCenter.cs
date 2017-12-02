09e486097c766a6d0af4f2392fc6ee52b8533f85
Szymon Piórkowski <szymmon123@gmail.com>
2017-12-02 15:30:07 +0100

    Fixed camera position.

--- Tankz/Assets/Scripts/CameraCenter.cs
+++ Tankz/Assets/Scripts/CameraCenter.cs
@@ -16,10 +16,12 @@ public class CameraCenter : MonoBehaviour {
 
     private void Start()
     {
-        mainCamera.transform.position = new Vector3(grid.nodeSize * grid.gridSize.x /2, grid.nodeSize * grid.gridSize.y / 2, - 10);
+        mainCamera.transform.position = new Vector3(grid.nodeSize * grid.gridSize.x /2.0f - grid.nodeSize/2.0f , grid.nodeSize * grid.gridSize.y / 2.0f - grid.nodeSize / 2.0f, - 10);
     }
 
     void Update () {
         mainCamera.orthographicSize -= Input.mouseScrollDelta.y * scrollSpeed;
+        if (mainCamera.orthographicSize < 1)
+            mainCamera.orthographicSize = 1;
     }
 }
