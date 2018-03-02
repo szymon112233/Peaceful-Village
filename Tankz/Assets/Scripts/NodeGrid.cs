2df6f87161be49da1678a65bcb7c2b075d2dd680
Arosos <wierzba96@o2.pl>
2018-03-02 20:56:28 +0100

    Added basic map loader. For now, only wall prefab from MapCreatorTileset is allowed.

--- Tankz/Assets/Scripts/NodeGrid.cs
+++ Tankz/Assets/Scripts/NodeGrid.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class NodeGrid : MonoBehaviour {
-
+    
     public Vector2Int gridSize = new Vector2Int();
     public float nodeSize = 0.0f;
     public GameObject nodePrefab = null;
@@ -13,20 +15,68 @@ public class NodeGrid : MonoBehaviour {
     public int numberOfWallsToGenerate = 10;
     public int numberOfEnemiesToGenerate = 1;
     public MapNode[,] MapNodes = null;
+    public GameObject[] environmentPrefabs = null;
 
-	
-	void Start () {
+    public bool spawnPlayers, spawnEnemies;
+
+    List<GameObject> players = new List<GameObject>();
+    List<GameObject> enemies = new List<GameObject>();
+
+    public void GenerateRandom()
+    {
+        Clear();
         GenerateGrid();
         GenerateOuterWalls();
         GenerateRandomWalls(numberOfWallsToGenerate);
         GeneratePlayers();
-	    GenerateEnemies();
-	}
-	
-	void GenerateGrid()
+        GenerateEnemies();
+    }
+
+    public void LoadMap()
+    {
+        Clear();
+        string path = EditorUtility.OpenFilePanel("Select map file", "", "csv");
+        if (File.Exists(path))
+        {
+            string content = File.ReadAllText(path);
+            string[] lines = content.Split('\n');
+            gridSize.x = lines[0].Split(',').Length;
+            gridSize.y = lines.Length - 1;
+            GenerateGrid();
+            GenerateOuterWalls();
+            for (int i = gridSize.y - 1; i >= 0; i--)
+            {
+                string[] indices = lines[gridSize.y - 1 - i].Split(',');
+                int index;
+                for (int j = 0; j < gridSize.x; j++)
+                {
+                    if (MapNodes[j, i].objectOnNode == null && (index = int.Parse(indices[j])) != -1)
+                        MapNodes[j, i].objectOnNode = Instantiate(environmentPrefabs[index], new Vector3(j * nodeSize, i * nodeSize, 1.0f), new Quaternion(), gameObject.transform);
+                }
+            }
+            if (spawnPlayers)
+            GeneratePlayers();
+            if (spawnEnemies)
+            GenerateEnemies();
+        }
+        else
+            Debug.LogError("Can't read from file!");
+    }
+
+    void Clear()
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        foreach (GameObject player in players)
+            Destroy(player);
+        foreach (GameObject enemy in enemies)
+            Destroy(enemy);
+    }
+
+    void GenerateGrid()
     {
         MapNodes = new MapNode[gridSize.x, gridSize.y];
-        for (int i = (gridSize.y - 1) ; i>=0 ; i--)
+        for (int i = gridSize.y - 1; i >= 0; i--)
         {
             for (int j = 0; j < gridSize.x; j++)
             {
@@ -89,6 +139,7 @@ public class NodeGrid : MonoBehaviour {
             int[] x =  { 2, gridSize.x - 2 };
             int[] y =  { 2, gridSize.y - 2 };
             GameObject player = Instantiate(playerPrefabs[counter], new Vector3(x[counter] * nodeSize, y[counter++] * nodeSize, 0), new Quaternion());
+            players.Add(player);
         }
     }
     
@@ -96,7 +147,8 @@ public class NodeGrid : MonoBehaviour {
     {
         for (int i = 0; i < numberOfEnemiesToGenerate; i++)
         {
-            Instantiate(enemyPrefab, new Vector3(Random.Range(8,(gridSize.x - 1) * nodeSize ), Random.Range(8,(gridSize.y - 1) * nodeSize ), 0), new Quaternion());
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(Random.Range(8,(gridSize.x - 1) * nodeSize ), Random.Range(8,(gridSize.y - 1) * nodeSize ), 0), new Quaternion());
+            enemies.Add(enemy);
         }
     }
 }
